72909809ba74017030e60d115deda5d63a63d523
Ritozz <guransasha@gmail.com>
2021-07-21 01:23:42 +0200

    modern header

--- logoShop/js/app.js
+++ logoShop/js/app.js
@@ -4,48 +4,62 @@ function email_test(input) {
 let menuParents = document.querySelectorAll('.menu-sidebar__sublink');
 
 for (let i = 0; i < menuParents.length; i++) {
-   const menuParent = menuParents[i];
-   menuParent.addEventListener('mouseenter', function (e) {
-      menuParent.classList.add('_active');
-   });
-   menuParent.addEventListener('mouseleave', function (e) {
-      menuParent.classList.remove('_active');
-   })
+    const menuParent = menuParents[i];
+    menuParent.addEventListener('mouseenter', function (e) {
+        menuParent.classList.add('_active');
+    });
+    menuParent.addEventListener('mouseleave', function (e) {
+        menuParent.classList.remove('_active');
+    })
 }
 
 let menuBurger = document.querySelector('.menu-sidebar__menu-icon');
 let menuBurgerBody = document.querySelector('.menu-sidebar__body');
 
 menuBurger.addEventListener('click', function (e) {
-   menuBurger.classList.toggle('_active');
-   _slideToggle(menuBurgerBody);
+    menuBurger.classList.toggle('_active');
+    _slideToggle(menuBurgerBody);
 });
 
+
+//*...........FIRST VARIANT.........................
 let categoriesBtn = document.querySelector('.categories__title');
 let categoriesBody = document.querySelector('.categories__list');
 
 categoriesBtn.addEventListener('click', function (e) {
-   _slideToggle(categoriesBody);
-   categoriesBtn.parentElement.classList.toggle('_active');
+    _slideToggle(categoriesBody);
+    categoriesBtn.classList.toggle('_active');
 });
-
 
 
 let categoriesItem = document.querySelectorAll('.categories__item');
 
 categoriesItem.forEach(item => {
-   item.addEventListener('change', function (e) {
-      item.classList.toggle('_active');
-      let categoriesActiveItems = document.querySelectorAll('.categories__item._active');
-      if (categoriesActiveItems.length) {
-         categoriesBtn.classList.add('_categories');
-         let quantiti = document.querySelector('.quantiti');
-         quantiti.innerHTML = `${quantiti.getAttribute('data-text')} ${categoriesActiveItems.length}`;
-      } else {
-         categoriesBtn.classList.remove('_categories');
-      }
-   });
+    item.addEventListener('change', function (e) {
+        item.classList.toggle('_active');
+        let categoriesActiveItems = document.querySelectorAll('.categories__item._active');
+        if (categoriesActiveItems.length) {
+            categoriesBtn.classList.add('_categories');
+            let quantiti = document.querySelector('.quantiti');
+            quantiti.innerHTML = `${quantiti.getAttribute('data-text')} ${categoriesActiveItems.length}`;
+        } else {
+            categoriesBtn.classList.remove('_categories');
+        }
+    });
 })
+
+
+
+//*...........SECOND VARIANT.........................
+
+// let categoriesBtn = document.querySelector('.search-page__categories');
+// let categoriesBody = document.querySelector('.categories__list');
+
+// categoriesBtn.addEventListener('click', function (e) {
+//    _slideToggle(categoriesBody);
+//    categoriesBtn.classList.toggle('_active');
+//    // categoriesBody.classList.toggle('_active');
+// });
 
 // let categoriesTitle = document.querySelector('.categories__title');
 // let categoriesItems = document.querySelectorAll('.categories__item');
@@ -59,13 +73,28 @@ categoriesItem.forEach(item => {
 
 //       if (categoriesActiveItems.length > 0) {
 //          categoriesTitle.classList.add('_categories');
-//          console.log('added');
+//          let quantiti = document.querySelector('.quantiti');
+//          quantiti.innerHTML = `${quantiti.getAttribute('data-text')} ${categoriesActiveItems.length}`;
 //       } else {
 //          categoriesTitle.classList.remove('_categories');
-//          console.log('removed');
 //       }
 //    });
 // }
+
+//*                                cart                                               
+
+let cartParent = document.querySelector('[data-cart]');
+let cart = document.querySelector('.info-header__cart');
+let headerLine = document.querySelector('[data-cartmove]');
+
+window.addEventListener('resize', () => {
+    if (document.documentElement.clientWidth < 550) {
+        cart.remove();
+        headerLine.append(cart);
+    } else {
+        cartParent.append(cart);
+    }
+});
 // Dynamic Adapt v.1
 // HTML data-da="where(uniq class name),when(breakpoint),position(digi)"
 // e.x. data-da=".item,992,2"
