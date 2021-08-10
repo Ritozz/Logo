4e1e6e0d3476b3a439ec91fbae47df9df9dbfef2
Ritozz <guransasha@gmail.com>
2021-08-11 01:39:15 +0200

    second lesson mod

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,25 +1,23 @@
 let menuParents = document.querySelectorAll('.menu-sidebar__sublink');
 
-for (let i = 0; i < menuParents.length; i++) {
-    const menuParent = menuParents[i];
-    menuParent.addEventListener('mouseenter', function (e) {
+menuParents.forEach(menuParent => {
+    menuParent.addEventListener('mouseenter', () => {
         menuParent.classList.add('_active');
     });
-    menuParent.addEventListener('mouseleave', function (e) {
+    menuParent.addEventListener('mouseleave', () => {
         menuParent.classList.remove('_active');
-    })
-}
+    });
+})
 
 let menuBurger = document.querySelector('.menu-sidebar__menu-icon');
 let menuBurgerBody = document.querySelector('.menu-sidebar__body');
 
-menuBurger.addEventListener('click', function (e) {
+menuBurger.addEventListener('click', function () {
     menuBurger.classList.toggle('_active');
-    _slideToggle(menuBurgerBody);
+    menuBurgerBody.classList.toggle('_active');
 });
 
 
-//*...........FIRST VARIANT.........................
 let categoriesBtn = document.querySelector('.categories__title');
 let categoriesBody = document.querySelector('.categories__list');
 
@@ -28,11 +26,10 @@ categoriesBtn.addEventListener('click', function (e) {
     categoriesBtn.classList.toggle('_active');
 });
 
-
 let categoriesItem = document.querySelectorAll('.categories__item');
 
 categoriesItem.forEach(item => {
-    item.addEventListener('change', function (e) {
+    item.addEventListener('change', function () {
         item.classList.toggle('_active');
         let categoriesActiveItems = document.querySelectorAll('.categories__item._active');
         if (categoriesActiveItems.length) {
@@ -44,39 +41,6 @@ categoriesItem.forEach(item => {
         }
     });
 })
-
-
-
-//*...........SECOND VARIANT.........................
-
-// let categoriesBtn = document.querySelector('.search-page__categories');
-// let categoriesBody = document.querySelector('.categories__list');
-
-// categoriesBtn.addEventListener('click', function (e) {
-//    _slideToggle(categoriesBody);
-//    categoriesBtn.classList.toggle('_active');
-//    // categoriesBody.classList.toggle('_active');
-// });
-
-// let categoriesTitle = document.querySelector('.categories__title');
-// let categoriesItems = document.querySelectorAll('.categories__item');
-
-// for (let i = 0; i < categoriesItems.length; i++) {
-//    let categoriesItem = categoriesItems[i];
-//    categoriesItem.addEventListener('change', function (e) {
-//       categoriesItem.classList.toggle('_active');
-
-//       let categoriesActiveItems = document.querySelectorAll('.categories__item._active');
-
-//       if (categoriesActiveItems.length > 0) {
-//          categoriesTitle.classList.add('_categories');
-//          let quantiti = document.querySelector('.quantiti');
-//          quantiti.innerHTML = `${quantiti.getAttribute('data-text')} ${categoriesActiveItems.length}`;
-//       } else {
-//          categoriesTitle.classList.remove('_categories');
-//       }
-//    });
-// }
 
 //*                                cart                                               
 
